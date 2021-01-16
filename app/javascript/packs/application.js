84e1465f4a7180f49a22d6a7dd063bcbc4c1dc66
Ubuntu <ubuntu@ip-172-31-5-72.ap-northeast-1.compute.internal>
2021-01-16 05:54:58 +0000

    ヘッダーバー　デザイン追加

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,6 +9,7 @@ require("@rails/activestorage").start()
 require("channels")
 import 'bootstrap'
 import '../src/application.scss'
+const images = require.context('../images/', true)
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
